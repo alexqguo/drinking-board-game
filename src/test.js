146baa7404e7bcd08c38962b18e83dbb2ec247a0
Alex Guo <alexguo@amazon.com>
2019-07-22 10:53:38 -0400

    add some notes

--- src/test.js
+++ src/test.js
@@ -7,8 +7,28 @@
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
-  canvas.width = 1000;
-  canvas.height = 1000;
-  canvas.style.background = 'url(./pokemon.png)';
-  canvas.style.backgroundSize = '100% 100%';
+  canvas.width = 1200;
+  canvas.height = 1200;
+  
+  // Option 1: css background image
+  // probably will not handle scaling very easily
+  // canvas.style.background = 'url(./pokemon.png)';
+  // canvas.style.backgroundSize = '100% 100%';
+
+  // Option 2: background within the canvas itself
+  const img = new Image();
+  img.src = './pokemon.png';
+  img.addEventListener('load', () => {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  });
+
+  // assumes a square board
+  function calculateSize() {
+    const size = Math.min(window.innerHeight, window.innerWidth);
+    canvas.width = size;
+    canvas.height = size;
+  }
+
+  // window.addEventListener('resize', calculateSize);
+  // calculateSize();
 })();
