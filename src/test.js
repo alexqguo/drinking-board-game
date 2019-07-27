096071f57c996076d0dd344f41547ecfdbc23007
Alex Guo <alexguo@amazon.com>
2019-07-27 19:37:56 -0400

    draw some basic test points

--- src/test.js
+++ src/test.js
@@ -6,31 +6,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 1200;
-canvas.height = 1200;
-
 // Option 1: css background image
 // probably will not handle scaling very easily
-// canvas.style.background = 'url(./pokemon.png)';
-// canvas.style.backgroundSize = '100% 100%';
-
-// Option 2: background within the canvas itself
-const img = new Image();
-img.src = './public/pokemon/index.png';
-img.addEventListener('load', () => {
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-});
-
-// assumes a square board
-function calculateSize() {
-  const size = Math.min(window.innerHeight, window.innerWidth);
-  canvas.width = size;
-  canvas.height = size;
-}
-
-// window.addEventListener('resize', calculateSize);
-// calculateSize();
-
 const testPosition = [{
   "x": 260,
   "y": 1770
@@ -44,10 +21,28 @@ const testPosition = [{
   "x": 450,
   "y": 1770
 }];
-ctx.fillStyle = '#ff2626';
-testPosition.forEach(pos => {
-  ctx.beginPath();
-  ctx.arc(50, 500, 10, 0, Math.PI * 2, true);
-  // ctx.stroke();
-  ctx.fill();
+const imgSrc = 'public/pokemon/index.png';
+const img = new Image();
+img.src = imgSrc;
+img.addEventListener('load', () => {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  canvas.style.background = `url(${imgSrc})`;
+  canvas.style.backgroundSize = '100% 100%';
+
+  testPosition.forEach(pos => {
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, 5, 0, Math.PI * 2, true);
+    ctx.fill();
+  });
 });
+
+// Option 2: background within the canvas itself
+// const img = new Image();
+// img.src = './public/pokemon/index.png';
+// img.addEventListener('load', () => {
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+//   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+// });
+
