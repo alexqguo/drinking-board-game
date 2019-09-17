f96b17ecd40d78c5c020065375956393a4decbe0
Alex Guo <alexguo@amazon.com>
2019-09-16 20:10:20 -0400

    Remove override

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -166,7 +166,6 @@ class Game {
     //   numSpacesToAdvance = 4;
     //   (window as any).asdf = true;
     // }
-    numSpacesToAdvance = 1;
 
     if (numSpacesToAdvance > 0) {
       // Consider fixing this naming. This doesn't actually move anything in the UI
