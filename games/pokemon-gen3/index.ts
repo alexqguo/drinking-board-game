dab7f29d059c79e532534374f45151d622551735
Alex Guo <alexguo@amazon.com>
2019-10-24 18:25:29 -0400

    Update gen3 to have proper trainer battles

--- games/pokemon-gen3/index.ts
+++ games/pokemon-gen3/index.ts
@@ -19,5 +19,61 @@ const starterStrengths = {
 const w = window as any;
 
 if (w.drinking) {
-  
+  const { Game, GameEvents, events } = w.drinking;
+
+  GameEvents.on(events.GAME_START, (next: Function) => {
+    const frag: DocumentFragment = document.createDocumentFragment();
+    const pokemonSelector: HTMLElement = document.createElement('pokemon-selector');
+    const playerNames: string[] = Game.players.map((p: any) => p.name);
+    pokemonSelector.setAttribute('playerNames', JSON.stringify(playerNames));
+    pokemonSelector.setAttribute('starterNames', JSON.stringify(Object.values(starterNames)));
+    pokemonSelector.addEventListener('pokemon-selected', (e: CustomEvent) => {
+      e.detail.pokemon.forEach((pokemon: string, i: number) => {
+        Game.players[i].custom.pokemon = pokemon;
+      });
+      Game.modal.close();
+      next();
+    });
+
+    frag.appendChild(pokemonSelector);
+    Game.modal.openWithFragment('Choose your Pokemon!', frag);
+    Game.modal.disableClose();
+  });
+
+  GameEvents.on(events.MOVE_END, (next: Function) => {
+    const currentTile = Game.board.tiles[Game.currentPlayer.currentTileIndex];
+    const playersAtCurrentTile: Set<Object> = currentTile.currentPlayers;
+
+    if (playersAtCurrentTile.size >= 2 && !currentTile.isMandatory) {
+      triggerTrainerBattle(playersAtCurrentTile)
+        .then(() => { next() });
+    } else {
+      next();
+    }
+  });
+
+  function triggerTrainerBattle(players: Set<Object>): Promise<void> {
+    return new Promise(resolve => {
+      const frag: DocumentFragment = document.createDocumentFragment();
+      const battle: HTMLElement = document.createElement('trainer-battle');
+      const componentArgs = [...players].map((p: any) => {
+        return {
+          playerName: p.name,
+          starterName: p.custom.pokemon,
+        }
+      });
+      battle.setAttribute('victorySrc', 'public/pokemon-gen3/victory.mp3');
+      battle.setAttribute('battleSrc', 'public/pokemon-gen3/battle.mp3');
+      battle.setAttribute('starterStrengths', JSON.stringify(starterStrengths));
+      battle.setAttribute('data', JSON.stringify(componentArgs));
+      battle.addEventListener('battle-ended', (e: CustomEvent) => {
+        Game.modal.enableClose();
+        resolve();
+      });
+
+      frag.appendChild(battle);
+      Game.modal.openWithFragment('Trainer battle!', frag);
+      Game.modal.disableClose();
+    });
+  }
 }
