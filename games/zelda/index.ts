358176b0e027ce0d7078e389e70912d94e9b9c01
Alex Guo <alexguo@amazon.com>
2020-02-21 17:26:40 -0500

    Some progress on the zelda schema

--- games/zelda/index.ts
+++ games/zelda/index.ts
@@ -1,1 +1,27 @@
 console.log('hello from the zelda game');
+
+/*
+todo: do not repeat mandatory spaces
+
+deku tree: 4, 5, 6
+dodongo: 14, 15, 16
+jabu jabu:
+forest temple:
+fire temple:
+water temple:
+shadow temple:
+spirit temple:
+ganon's castle:
+temple of time:
+*/
+
+const w = window as any;
+
+if (w.drinking) {
+  const { Game, GameEvents, events } = w.drinking;
+
+  GameEvents.on(events.MOVE_END, (next: Function) => {
+    if (Game.currentPlayer.currentTileIndex === 4) {
+    }
+  });
+}
